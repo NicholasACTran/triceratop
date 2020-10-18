555d020f6cf9008b17320c6e2e459013b7d70922
NicholasACTran <nicholas.ac.tran@gmail.com>
2020-10-18 11:32:47 -0700

    Added parsing for descriptions of nodes

--- util/parser.ts
+++ util/parser.ts
@@ -1,8 +1,8 @@
-const FEATURE : string = 'Feature';
-const SCENARIO : string = 'Scenario';
-const GIVEN : string = 'Given';
-const WHEN : string = 'When';
-const THEN : string = 'Then';
+const FEATURE : string = 'Feature:';
+const SCENARIO : string = 'Scenario:';
+const GIVEN : string = 'Given:';
+const WHEN : string = 'When:';
+const THEN : string = 'Then:';
 
 class SyntaxNode {
   type: String;
@@ -16,57 +16,90 @@ class SyntaxNode {
 
 const ParseThenNode =
   (tokens: Array<string>, nodes: Array<SyntaxNode>) => {
-    nodes.push(new SyntaxNode(THEN, ''));
+    let then_description = '';
     tokens.shift();
-    if (tokens.length === 0 || tokens[0] === '') {
-      return;
-    }
-    else if (tokens[0] === THEN) {
-      ParseThenNode(tokens, nodes);
-    } else if (tokens[0] === SCENARIO) {
-      ParseScenarioNode(tokens, nodes);
-    } else {
-      throw 'Expected "Then", "Scenario", or EOF after "Then"';
+    while (true) {
+      if (tokens.length === 0 || tokens[0] === '') {
+        nodes.push(new SyntaxNode(THEN, then_description));
+        return;
+      }
+      else if (tokens[0] === THEN) {
+        nodes.push(new SyntaxNode(THEN, then_description));
+        ParseThenNode(tokens, nodes);
+      } else if (tokens[0] === SCENARIO) {
+        nodes.push(new SyntaxNode(THEN, then_description));
+        ParseScenarioNode(tokens, nodes);
+      }
+
+      then_description = (then_description === '') ? `${tokens.shift()}`
+                              : `${then_description} ${tokens.shift()}`;
     }
   }
 
 const ParseWhenNode =
   (tokens: Array<string>, nodes: Array<SyntaxNode>) => {
-    nodes.push(new SyntaxNode(WHEN, ''));
+    let when_description = '';
     tokens.shift();
-    if (tokens[0] === WHEN) {
-      ParseWhenNode(tokens, nodes);
-    } else if (tokens[0] === THEN) {
-      ParseThenNode(tokens, nodes);
-    } else {
-      throw 'Expected "When" or "Then" after "When"';
+    while (true) {
+      if (tokens[0] === WHEN) {
+        nodes.push(new SyntaxNode(WHEN, when_description));
+        ParseWhenNode(tokens, nodes);
+        break;
+      } else if (tokens[0] === THEN) {
+        nodes.push(new SyntaxNode(WHEN, when_description));
+        ParseThenNode(tokens, nodes);
+        break;
+      } else if (tokens[0] === '') {
+        throw 'Expected "When" or "Then" after "When"';
+      }
+
+      when_description = (when_description === '') ? `${tokens.shift()}`
+                              : `${when_description} ${tokens.shift()}`;
     }
   }
 
 const ParseGivenNode =
   (tokens: Array<string>, nodes: Array<SyntaxNode>) => {
-    nodes.push(new SyntaxNode(GIVEN, ''));
+    let given_description = '';
     tokens.shift();
-    if (tokens[0] === GIVEN) {
-      ParseGivenNode(tokens, nodes);
-    } else if (tokens[0] === WHEN) {
-      ParseWhenNode(tokens, nodes);
-    } else {
-      throw 'Expected "Given" or "When" after "Given"';
+    while (true) {
+      if (tokens[0] === GIVEN) {
+        nodes.push(new SyntaxNode(GIVEN, given_description));
+        ParseGivenNode(tokens, nodes);
+        break;
+      } else if (tokens[0] === WHEN) {
+        nodes.push(new SyntaxNode(GIVEN, given_description));
+        ParseWhenNode(tokens, nodes);
+        break;
+      } else if (tokens[0] === ''){
+        throw 'Expected "Given" or "When" after "Given"';
+      }
+
+      given_description = (given_description === '') ? `${tokens.shift()}`
+                              : `${given_description} ${tokens.shift()}`;
     }
   }
 
 const ParseScenarioNode =
   (tokens: Array<string>, nodes: Array<SyntaxNode>) => {
     if (tokens[0] === SCENARIO) {
-      nodes.push(new SyntaxNode(SCENARIO, ''));
+      let scenarion_description = '';
       tokens.shift();
-      if (tokens[0] === GIVEN) {
-        ParseGivenNode(tokens, nodes);
-      } else if (tokens[0] === WHEN) {
-        ParseWhenNode(tokens, nodes);
-      } else {
-        throw 'Expected "Given" or "When" after "Scenario"';
+      while (true) {
+        if (tokens[0] === GIVEN) {
+          nodes.push(new SyntaxNode(SCENARIO, scenarion_description));
+          ParseGivenNode(tokens, nodes);
+          break;
+        } else if (tokens[0] === WHEN) {
+          nodes.push(new SyntaxNode(SCENARIO, scenarion_description));
+          ParseWhenNode(tokens, nodes);
+          break;
+        } else if (tokens[0] === '') {
+          throw 'Expected "Given" or "When" after "Scenario"';
+        }
+
+        scenarion_description = (scenarion_description === '') ? `${tokens.shift()}`
+                                : `${scenarion_description} ${tokens.shift()}`;
       }
     } else {
       throw 'Expected "Scenario" after "Feature"';
@@ -76,8 +109,17 @@ const ParseScenarioNode =
 const ParseFeatureNode =
   (tokens: Array<string>, nodes: Array<SyntaxNode>) : Array<SyntaxNode> => {
     if (tokens[0] === FEATURE) {
-      nodes.push(new SyntaxNode(FEATURE, ''));
+      let feature_description = '';
       tokens.shift();
+      while (tokens[0] !== SCENARIO) {
+        if (tokens[0] === '') {
+          throw 'Expected "Scenario" after "Feature"';
+        }
+
+        feature_description = (feature_description === '') ? `${tokens.shift()}`
+                                : `${feature_description} ${tokens.shift()}`;
+      }
+      nodes.push(new SyntaxNode(FEATURE, feature_description));
       ParseScenarioNode(tokens, nodes);
     } else {
       throw 'Feature file must start with "Feature:"';
@@ -94,9 +136,9 @@ const GenerateSyntaxList = (text: string): Array<SyntaxNode> => {
 }
 
 export async function Parser (feature_file : string) : Promise<string> {
-  //Todo: Implement parsing logic
+  // TODO: Implement parsing logic
+  // TODO: Figure out parsing error when using an incorrect Key term
   const text = await Deno.readTextFile(feature_file);
   const parsed_syntax = GenerateSyntaxList(text);
-
   return Promise.resolve(text);
 }
