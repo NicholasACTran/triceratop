4818adc1e276b0b1a6d2128d17018323b91c62bc
NicholasACTran <nicholas.ac.tran@gmail.com>
2020-10-20 19:19:55 -0700

    Created semantic scaffolding

--- util/parser.ts
+++ util/parser.ts
@@ -1,9 +1,10 @@
 import { SyntaxNode, GenerateSyntaxList } from './syntax.ts';
+import { CreateSemanticText } from './semantic.ts';
 
 export async function Parser (feature_file : string) : Promise<string> {
   // TODO: Implement parsing logic
   // TODO: Figure out parsing error when using an incorrect Key term
   const text = await Deno.readTextFile(feature_file);
   const parsed_syntax : Array<SyntaxNode> = GenerateSyntaxList(text);
-  return Promise.resolve(text);
+  return Promise.resolve(CreateSemanticText(parsed_syntax));
 }
---
+++ util/semantic.ts
@@ -0,0 +1,7 @@
+import { SyntaxNode } from './syntax.ts';
+
+//TODO: Implement semantics
+export function CreateSemanticText(nodes : Array<SyntaxNode>) : string {
+  let text = '';
+  return text;
+}
