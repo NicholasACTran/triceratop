b573dec7171159446ef21d40ce51ff6637c7582e
NicholasACTran <nicholas.ac.tran@gmail.com>
2020-10-21 19:58:36 -0700

    Added more scaffolding for semantics

--- util/semantic.ts
+++ util/semantic.ts
@@ -3,5 +3,19 @@ import { SyntaxNode } from './syntax.ts';
 //TODO: Implement semantics
 export function CreateSemanticText(nodes : Array<SyntaxNode>) : string {
   let text = '';
+  for (const node of nodes) {
+    switch(node.type) {
+      case 'Feature':
+        break;
+      case 'Scenario':
+        break;
+      case 'Example':
+        break;
+      case 'When':
+        break;
+      case 'Then':
+        break;
+    }
+  }
   return text;
 }
