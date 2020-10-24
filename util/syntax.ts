0d8c427d7f7b2e3ab36037b4f1f28926ca6c96c5
NicholasACTran <nicholas.ac.tran@gmail.com>
2020-10-23 21:03:09 -0700

    Added parsing for And Nodes

--- util/syntax.ts
+++ util/syntax.ts
@@ -5,6 +5,7 @@ const GIVEN : string = 'Given:';
 const WHEN : string = 'When:';
 const THEN : string = 'Then:';
 const BACKGROUND : string = 'Background';
+const AND : string = 'And';
 
 export class SyntaxNode {
   type: String;
@@ -16,6 +17,55 @@ export class SyntaxNode {
   }
 }
 
+const ParseAndNode =
+  (tokens: Array<string>, nodes: Array<SyntaxNode>, context: string) => {
+    let and_description = '';
+    tokens.shift();
+    while (true) {
+      if (tokens.length === 0 || tokens[0] === '') {
+        if (context === THEN) {
+          nodes.push(new SyntaxNode(AND, and_description));
+          return;
+        } else {
+          throw `EOF not expected after "And" under context ${context}`;
+        }
+      }
+
+      if (tokens[0] === THEN) {
+        if (context === WHEN) {
+          nodes.push(new SyntaxNode(AND, and_description));
+          ParseThenNode(tokens, nodes);
+          return;
+        } else {
+          throw `Then not expected after "And" under context ${context}`;
+        }
+      } else if (tokens[0] === WHEN) {
+        if (context === GIVEN) {
+          nodes.push(new SyntaxNode(AND, and_description));
+          ParseWhenNode(tokens, nodes);
+          return;
+        } else {
+          throw `When not expected after "And" under context ${context}`;
+        }
+      } else if (tokens[0] === SCENARIO || tokens[0] === EXAMPLE) {
+        if (context === THEN) {
+          nodes.push(new SyntaxNode(AND, and_description));
+          ParseScenarioExampleNode(tokens, nodes, tokens[0]);
+          return;
+        } else {
+          throw `${tokens[0]} not expected after "AND" under context ${context}`;
+        }
+      } else if (tokens[0] === AND) {
+        nodes.push(new SyntaxNode(AND, and_description));
+        ParseAndNode(tokens, nodes, context);
+        return;
+      }
+
+      and_description = (and_description === '') ? `${tokens.shift()}`
+                              : `${and_description} ${tokens.shift()}`;
+    }
+  }
+
 const ParseThenNode =
   (tokens: Array<string>, nodes: Array<SyntaxNode>) => {
     let then_description = '';
@@ -25,18 +75,24 @@ const ParseThenNode =
         nodes.push(new SyntaxNode(THEN, then_description));
         return;
       }
-      else if (tokens[0] === THEN) {
+      else if (tokens[0] === AND) {
+        nodes.push(new SyntaxNode(THEN, then_description));
+        ParseAndNode(tokens, nodes, THEN);
+        return;
+      } else if (tokens[0] === THEN) {
         nodes.push(new SyntaxNode(THEN, then_description));
         ParseThenNode(tokens, nodes);
+        return;
       } else if (tokens[0] === SCENARIO || tokens[0] === EXAMPLE) {
         nodes.push(new SyntaxNode(THEN, then_description));
         ParseScenarioExampleNode(tokens, nodes, tokens[0]);
+        return;
       }
 
       then_description = (then_description === '') ? `${tokens.shift()}`
                               : `${then_description} ${tokens.shift()}`;
     }
-  }
+  };
 
 const ParseWhenNode =
   (tokens: Array<string>, nodes: Array<SyntaxNode>) => {
@@ -46,11 +102,15 @@ const ParseWhenNode =
       if (tokens[0] === WHEN) {
         nodes.push(new SyntaxNode(WHEN, when_description));
         ParseWhenNode(tokens, nodes);
-        break;
+        return;
+      } else if (tokens[0] === AND) {
+        nodes.push(new SyntaxNode(WHEN, when_description));
+        ParseAndNode(tokens, nodes, WHEN);
+        return;
       } else if (tokens[0] === THEN) {
         nodes.push(new SyntaxNode(WHEN, when_description));
         ParseThenNode(tokens, nodes);
-        break;
+        return;
       } else if (tokens[0] === '') {
         throw 'Expected "When" or "Then" after "When"';
       }
@@ -58,7 +118,7 @@ const ParseWhenNode =
       when_description = (when_description === '') ? `${tokens.shift()}`
                               : `${when_description} ${tokens.shift()}`;
     }
-  }
+  };
 
 const ParseGivenNode =
   (tokens: Array<string>, nodes: Array<SyntaxNode>) => {
@@ -68,11 +128,15 @@ const ParseGivenNode =
       if (tokens[0] === GIVEN) {
         nodes.push(new SyntaxNode(GIVEN, given_description));
         ParseGivenNode(tokens, nodes);
-        break;
+        return;
+      } else if (tokens[0] === AND){
+        nodes.push(new SyntaxNode(GIVEN, given_description));
+        ParseAndNode(tokens, nodes, GIVEN);
+        return;
       } else if (tokens[0] === WHEN) {
         nodes.push(new SyntaxNode(GIVEN, given_description));
         ParseWhenNode(tokens, nodes);
-        break;
+        return;
       } else if (tokens[0] === ''){
         throw 'Expected "Given" or "When" after "Given"';
       }
@@ -80,13 +144,16 @@ const ParseGivenNode =
       given_description = (given_description === '') ? `${tokens.shift()}`
                               : `${given_description} ${tokens.shift()}`;
     }
-  }
+  };
 
 const ParseBackgroundGivenNode = (tokens: Array<string>) : SyntaxNode => {
     let given_description = '';
     tokens.shift();
     while (true) {
-      if (tokens[0] === GIVEN || tokens[0] === SCENARIO || tokens[0] === EXAMPLE) {
+      if (tokens[0] === GIVEN
+        || tokens[0] === AND
+        || tokens[0] === SCENARIO
+        || tokens[0] === EXAMPLE) {
         return (new SyntaxNode(GIVEN, given_description));
       } else if (tokens[0] === ''){
         throw 'Expected "Given" after "Given"';
@@ -106,11 +173,11 @@ const ParseScenarioExampleNode =
         if (tokens[0] === GIVEN) {
           nodes.push(new SyntaxNode(type, scenarion_description));
           ParseGivenNode(tokens, nodes);
-          break;
+          return;
         } else if (tokens[0] === WHEN) {
           nodes.push(new SyntaxNode(type, scenarion_description));
           ParseWhenNode(tokens, nodes);
-          break;
+          return;
         } else if (tokens[0] === '') {
           throw `Expected "Given" or "When" after "${type}"`;
         }
@@ -123,8 +190,9 @@ const ParseScenarioExampleNode =
     }
   };
 
+// TODO: Rework to implement AND and BUT nodes in Background
 const ParseBackgroundNode =
-  (tokens: Array<string>, nodes: Array<SyntaxNode>) : Array<SyntaxNode> => {
+  (tokens: Array<string>, nodes: Array<SyntaxNode>) => {
     tokens.shift();
     nodes.push(new SyntaxNode(BACKGROUND, ''));
     if (tokens[0] !== GIVEN) {
@@ -138,7 +206,6 @@ const ParseBackgroundNode =
       nodes.push(ParseBackgroundGivenNode(tokens));
     }
     ParseScenarioExampleNode(tokens, nodes, tokens[0]);
-    return nodes;
   };
 
 const ParseFeatureNode =
