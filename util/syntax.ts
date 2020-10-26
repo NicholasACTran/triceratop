daa43629e727783fe4b0e452a59bd7c15011a8e6
NicholasACTran <nicholas.ac.tran@gmail.com>
2020-10-26 15:50:17 -0700

    Parsing out Scenario Outline with Examples

--- util/syntax.ts
+++ util/syntax.ts
@@ -3,6 +3,7 @@ import { readLine } from './readline.ts';
 const FEATURE : string = 'Feature:';
 const SCENARIO : string = 'Scenario:';
 const EXAMPLE : string = 'Example:';
+const EXAMPLES : string = 'Examples:';
 const GIVEN : string = 'Given';
 const WHEN : string = 'When';
 const THEN : string = 'Then';
@@ -25,12 +26,64 @@ export class SyntaxNode {
   type: string;
   text: string;
   variables: string[];
+  exampleMap: any[];
 
-  constructor(type: string, text='', variables?:string[]) {
+  constructor(type: string, text='', variables?:string[], exampleMap?:any[]) {
     this.type = type;
     this.text = text;
     this.variables = variables ? variables : [];
+    this.exampleMap = exampleMap ? exampleMap : [];
   }
+}
+
+/**
+^ Parses Example Data Table
+*
+* @param generator the generator to grab the next lines until EOF or the next header
+* @param nodes the array to insert syntax nodes
+* @returns the header that the context will be set to for the next round of parsing
+*/
+const ParseExamplesNode = async (generator : AsyncGenerator<string>,
+                                  nodes: Array<SyntaxNode>) : Promise<string> => {
+  const examples : Array<any> = [];
+  const variables : Array<string> = [];
+
+  let g = await generator.next();
+  let header = RemovePadding(g.value).replace(/\|/g, '').split(' ');
+  for (const h of header) {
+    if (h !== '') variables.push(h);
+  }
+
+  while (true) {
+    g = await generator.next();
+    if (g.done) break;
+
+    let line = RemovePadding(g.value);
+    if (line[0] !== '|') {
+      const type = line.split(' ')[0];
+      if (line.length === 0) {}
+      else if (headers.includes(type)) {
+        nodes.push(new SyntaxNode(EXAMPLES, '', [], examples));
+        nodes.push(ParseHeaderNode(line, type));
+        return Promise.resolve(type);
+      } else throw 'Expected header key word after "Examples"';
+    }
+
+    const lines = line.replace(/\|/g, '').split(' ');
+    let counter = 0;
+    const exampleObj : any = {};
+    for (const token of lines) {
+      if (token !== '') {
+        exampleObj[variables[counter]] = token;
+        counter++;
+      }
+    }
+
+    examples.push(exampleObj);
+  }
+
+  nodes.push(new SyntaxNode(EXAMPLES, '', [], examples));
+  return Promise.resolve(SCENARIO_OUTLINE);
 }
 
 /**
@@ -39,7 +92,6 @@ export class SyntaxNode {
 * @param line line to parse
 * @param type type of node
 */
-
 const ParseFunctionNode = (line: string, type: string) : SyntaxNode => {
   const tokens = line.split(' ');
   const variables : Array<string> = [];
@@ -65,6 +117,15 @@ const ParseHeaderNode = (line: string, type: string) : SyntaxNode => {
   return new SyntaxNode(type, line.substring(line.indexOf(':') + 2));
 };
 
+// Removes tabs and trailing spaces from a line
+const RemovePadding = (line: string) : string => {
+  line = line.replace(/\t/g, '');
+  while (line[0] === ' ') {
+    line = line.substring(1);
+  }
+  return line;
+}
+
 /**
 * Given a feature file name, return a list of Syntax nodes that represent the
 * feature file. Following typical Gherkin rules
@@ -78,11 +139,14 @@ export async function GenerateSyntaxList(feature_file: string): Promise<Array<Sy
   // Index 0: The most recent header node
   // Index 1: The most recent, relevant function node (Given, When, Then)
   let context: [string, string] = ['', ''];
-  for await (let line of readLine(feature_file)) {
-    line = line.replace(/\t/g, '');
-    while (line[0] === ' ') {
-      line = line.substring(1);
+  const lines = await readLine(feature_file);
+  while (true) {
+    const generated = await lines.next();
+    if (generated.done) {
+      break;
     }
+    let line = generated.value;
+    line = RemovePadding(line);
     switch(line.split(' ')[0]) {
       case FEATURE:
         if (context[0] === '') {
@@ -123,6 +187,14 @@ export async function GenerateSyntaxList(feature_file: string): Promise<Array<Sy
             context[1] = '';
         } else throw '"SCENARIO" expected to follow "Feature", "Background", or "Then"';
         break;
+      case EXAMPLES:
+        if ([FEATURE, BACKGROUND].includes(context[0]) ||
+            (headers.includes(context[0]) &&
+              (context[1] === THEN || context[1] === ''))) {
+                context[0] = await ParseExamplesNode(lines, nodes);
+                context[1] = '';
+              } else throw '"Examples" expected to follow "Then"'
+        break;
       case GIVEN:
         if (context[0] === BACKGROUND ||
             ([SCENARIO, SCENARIO_OUTLINE, EXAMPLE].includes(context[0]) &&
@@ -133,7 +205,7 @@ export async function GenerateSyntaxList(feature_file: string): Promise<Array<Sy
         break;
       case WHEN:
         if ([SCENARIO, SCENARIO_OUTLINE, EXAMPLE].includes(context[0]) &&
-            [GIVEN, WHEN].includes(context[1])) {
+            [GIVEN, WHEN, ''].includes(context[1])) {
               nodes.push(ParseFunctionNode(line, WHEN));
               context[1] = WHEN;
             } else throw '"When" expected to follow "Given", "Scenario"';
