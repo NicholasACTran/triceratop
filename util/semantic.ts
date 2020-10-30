341a70c01c31b7b6b336ab4bbc053818e8e2b8de
Chris <chrng8@gmail.com>
2020-10-29 19:46:38 -0700

    Use backticks for test descriptions

--- util/semantic.ts
+++ util/semantic.ts
@@ -6,9 +6,9 @@ export function CreateSemanticText(nodes : Array<SyntaxNode>) : string {
   for (const node of nodes) {
     if (nodeTypes.includes(node.type)) {
       if (node.variables.length === 0) {
-        text = text + `${node.type}('${node.text}', () => {\n\n});\n\n`;
+        text = text + `${node.type}(\`${node.text}\`, () => {\n\n});\n\n`;
       } else {
-        text = text + `${node.type}('${node.text}', (`;
+        text = text + `${node.type}(\`${node.text}\`, (`;
         for (const variable of node.variables) {
           text = text + ` ${variable}, `;
         }
