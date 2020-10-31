b5efd0481d92137d7489bfab613bf62adf76586e
NicholasACTran <nicholas.ac.tran@gmail.com>
2020-10-30 17:13:38 -0700

    Added await/async semantics

--- util/semantic.ts
+++ util/semantic.ts
@@ -4,7 +4,7 @@ import { SyntaxNode } from './syntax.ts';
 const SETUP_MODULE = '../lib/mod.ts'
 
 export function CreateSemanticText(feature : string, nodes : Array<SyntaxNode>) : string {
-  let text = `TriceratopTest('${feature}', () => {\n`;
+  let text = `await TriceratopTest('${feature}', async () => {\n`;
   let importText = `import { `;
   const importList : Array<string> = ['TriceratopTest'];
   const nodeTypes = ['Given', 'When', 'Then', 'And', 'But'];
@@ -13,9 +13,9 @@ export function CreateSemanticText(feature : string, nodes : Array<SyntaxNode>) 
       if (!importList.includes(node.type)) importList.push(node.type);
 
       if (node.variables.length === 0) {
-        text = text + `\t${node.type}(\`${node.text}\`, () => {\n\n\t});\n\n`;
+        text = text + `\t${node.type}(\`${node.text}\`, async () => {\n\n\t});\n\n`;
       } else {
-        text = text + `\t${node.type}(\`${node.text}\`, (`;
+        text = text + `\t${node.type}(\`${node.text}\`, async (`;
         for (const variable of node.variables) {
           text = text + ` ${variable}, `;
         }
